6bcb452e3d45822c5ef54bb5a9b962d49f484813
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-23 22:34:19 -0300

    mais um

--- src/Restaurante.java
+++ src/Restaurante.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.list;
+import Cliente.Endereco;
+
 public class Restaurante {
     public static void main(String[] args) {
         System.out.printf("faz o l");
