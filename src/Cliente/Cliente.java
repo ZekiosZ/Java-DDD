d40ef5516d592c53e2f4d6911015ce256d0b2096
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-23 22:09:04 -0300

    Atualizando parte do projeto, getters em classe cliente e adicionando classe endereço no package cliente

--- src/Cliente/Cliente.java
+++ src/Cliente/Cliente.java
@@ -1,4 +1,33 @@
 package Cliente;
 
 public class Cliente {
+    private String id;
+    private String nome;
+    private String email;
+    private String telefone;
+
+    //criando um construtor
+    public Cliente(String nome, String id, String email, String telefone){
+        //criando um id unico aleatorio
+        this.id= java.util.UUID.randomUUID().toString();
+
+        this.nome= nome;
+        this.email= email;
+        this.telefone= telefone;
+        System.out.println("Cadastro do cliente "+nome +" realizado com sucesso");
+    }
+    //GETTERS - Consultando/ver dados
+    public String getId(){
+        return this.id;
+    }
+    public String getNome() {
+        return this.nome;
+    }
+    public String getEmail(){
+        return this.email;
+    }
+    public String getTelefone(){
+        return this.telefone;
+    }
+
 }
